dd38fea1efe459f3a5c2f4bc13cf4f43a144d127
Lasse Egebo <lae003@edu.zealand.dk>
2024-06-01 14:39:27 +0200

    IDBase class added

---
+++ IDBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZealandClimate
+{
+    internal class IDBase
+    {
+        private static int nextId = 1;
+        private int Id { get; set; }
+
+
+    }
+}
