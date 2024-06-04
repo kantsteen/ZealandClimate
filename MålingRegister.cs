4bb4f83b878dd60fb8c878d9e6f349ce9ca72e63
Lasse Egebo <lae003@edu.zealand.dk>
2024-06-04 21:05:15 +0200

    MålingRegister opg. 4

---
+++ MålingRegister.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZealandClimate
+{
+    internal class MålingRegister
+    {
+
+        private List<Måling> målinger = new List<Måling>();
+
+        public MålingRegister()
+        {
+            målinger = new List<Måling>();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@
 
             Måling måling1 = new Måling(DateTime.Now, 22.5, 445, 30, lokale1);
 
+            MålingRegister register = new MålingRegister();
+
             Console.WriteLine(lokale1);
             Console.WriteLine(måling1 + Environment.NewLine);
 
